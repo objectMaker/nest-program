84f7e7b0b164ea54976425ea96ef64a9687c1fcd
objectMaker <528154320@qq.com>
2023-06-24 04:00:04 +0800

    项目entity的问题

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,19 +5,20 @@ import * as dotenv from 'dotenv';
 import { readFileSync } from 'fs';
 //使用dotEnv读取环境变量里面的配置
 //通用的环境变量配置
-const defaultConfig = dotenv.parse(readFileSync('.env', { encoding: 'utf-8' }));
+const defaultConfig = dotenv.parse(readFileSync('.env'));
 //当前环境的环境变量配置
 const currentConfig = dotenv.parse(
-  readFileSync(`.env.${process.env.NODE_ENV}`, { encoding: 'utf-8' }),
+  readFileSync(`.env.${process.env.NODE_ENV}`),
 );
 //拿到当前环境变量的配置的类型
 type CONFIG_ENUM_VALUE = (typeof CONFIG_ENUM)[keyof typeof CONFIG_ENUM];
 //断言
+
 const ormConfig = {
-  ...currentConfig,
   ...defaultConfig,
+  ...currentConfig,
 } as unknown as Record<CONFIG_ENUM_VALUE, string | number>;
-//
+
 export function ormConfigGenerator() {
   return {
     type: ormConfig.TYPE,
@@ -26,7 +27,7 @@ export function ormConfigGenerator() {
     username: ormConfig.USER_NAME,
     password: ormConfig.PASSWORD,
     database: ormConfig.DATABASE,
-    entities: [join(__dirname + '../../../src/**/*.entity{.ts,.js}')],
+    entities: [join(__dirname + '/**/*.entity{.js,.ts}')],
     synchronize: process.env.NODE_ENV === DEVELOPMENT,
   };
 }
